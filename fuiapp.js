04554cab1c7692556ec2e15042dc02a35bab207f
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2022-02-17 18:36:14 +0530

    add code for card component
    Had to write same code for Card component  as Alert component. Because you cannot use same id twice in different elements. It should be unique.
    Class is out of option as all elements with same class name would behave same

--- fuiapp.js
+++ fuiapp.js
@@ -4,3 +4,10 @@ const close = document.querySelector('#alert-close');
 const callbackAlertHide = () => hide.style.display = "none";
 close.addEventListener('click', callbackAlertHide);
 // ALERT END
+
+// CARD START
+const cardHide = document.querySelector('#card-hide');
+const cardClose = document.querySelector('#card-close');
+const callbackCardHide = () => cardHide.style.display = "none";
+cardClose.addEventListener('click', callbackCardHide);
+// CARD END
